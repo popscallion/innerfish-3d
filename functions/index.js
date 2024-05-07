7fe176701eedc2435d03910d648351559f6218f5
L Fahn-Lai <l.fahnlai@gmail.com>
2024-05-07 16:22:45 -0400

    migrate to airtable PAT

--- functions/index.js
+++ functions/index.js
@@ -1,80 +1,94 @@
-const Airtable = require('airtable');
-const functions = require('firebase-functions');
-const cors = require('cors')({ origin: true });
+const Airtable = require("airtable");
+const functions = require("firebase-functions");
+const cors = require("cors")({ origin: true });
 
 const formatJSON = (data) => {
-  const formatted = []
+  const formatted = [];
   const template = {
-    "uid": "",
-    "url": "",
-    "scientific": "",
-    "common": "",
-    "group": "",
-    "chapter": [],
-    "showstopper": false,
-    "default": false,
-    "week": [],
-    "type": "",
-    "section": "",
-    "sectionIndex": "",
-    "caption": "",
-  }
+    uid: "",
+    url: "",
+    scientific: "",
+    common: "",
+    group: "",
+    chapter: [],
+    showstopper: false,
+    default: false,
+    week: [],
+    type: "",
+    section: "",
+    sectionIndex: "",
+    caption: "",
+  };
   for (let datum of data) {
     const newDatum = {
-      "uid": datum['UID'] ? datum['UID'] : template['UID'],
-      "url": datum['Link'] ? datum['Link'] : template['url'],
-      "scientific": datum['Scientific Name'] ? datum['Scientific Name'] : template['scientific'],
-      "common": datum['Other Name'] ? datum['Other Name'] : template['common'],
-      "group": datum['Group'] ? datum['Group'] : template['group'],
-      "chapter": datum['Chapter'] ? datum['Chapter'] : template['chapter'],
-      "showstopper": datum['Showstopper'] ? datum['Showstopper'] : template['showstopper'],
-      "default": datum['Default'] ? datum['Default'] : template['default'],
-      "week": datum['Week'] ? datum['Week'] : template['week'],
-      "type": datum['Type'] ? datum['Type'] : template['type'],
-      "section": datum['Section'] ? datum['Section'] : template['section'],
-      "sectionIndex": datum['SectionIndex'] ? datum['SectionIndex'] : template['sectionIndex'],
-      "caption": datum['Caption'] ? datum['Caption'] : template['caption'],
-    }
-    formatted.push(newDatum)
+      uid: datum["UID"] ? datum["UID"] : template["UID"],
+      url: datum["Link"] ? datum["Link"] : template["url"],
+      scientific: datum["Scientific Name"]
+        ? datum["Scientific Name"]
+        : template["scientific"],
+      common: datum["Other Name"] ? datum["Other Name"] : template["common"],
+      group: datum["Group"] ? datum["Group"] : template["group"],
+      chapter: datum["Chapter"] ? datum["Chapter"] : template["chapter"],
+      showstopper: datum["Showstopper"]
+        ? datum["Showstopper"]
+        : template["showstopper"],
+      default: datum["Default"] ? datum["Default"] : template["default"],
+      week: datum["Week"] ? datum["Week"] : template["week"],
+      type: datum["Type"] ? datum["Type"] : template["type"],
+      section: datum["Section"] ? datum["Section"] : template["section"],
+      sectionIndex: datum["SectionIndex"]
+        ? datum["SectionIndex"]
+        : template["sectionIndex"],
+      caption: datum["Caption"] ? datum["Caption"] : template["caption"],
+    };
+    formatted.push(newDatum);
   }
-  return formatted
-}
+  return formatted;
+};
 
 const loadData = () => {
-  const base = new Airtable({apiKey: functions.config().airtable.key}).base(functions.config().airtable.base)
+  const base = new Airtable({
+    endpointUrl: "https://api.airtable.com",
+    apiKey: functions.config().airtable.pat,
+  }).base(functions.config().airtable.base);
   return new Promise((resolve, reject) => {
-    const specimens = []
-    base('Specimens').select({
+    const specimens = [];
+    base("Specimens")
+      .select({
         fields: [
-          'Scientific Name',
-          'Other Name',
-          'UID',
-          'Link',
-          'Type',
-          'Showstopper',
-          'Default',
-          'Group',
-          'Chapter',
-          'Week',
-          'Section',
-          'SectionIndex',
-          'Caption'
+          "Scientific Name",
+          "Other Name",
+          "UID",
+          "Link",
+          "Type",
+          "Showstopper",
+          "Default",
+          "Group",
+          "Chapter",
+          "Week",
+          "Section",
+          "SectionIndex",
+          "Caption",
         ],
-        view: "Grid view"
-    }).eachPage(function page(records, fetchNextPage) {
-        records.forEach(function(record) {
-            specimens.push(record.fields)
-        });
-        fetchNextPage();
-    }, function done(err) {
-        if (err) {
-              reject(err);
-            } else {
-              resolve(specimens);
-            }
-    })
-  })
-}
+        view: "Grid view",
+      })
+      .eachPage(
+        (records, fetchNextPage) => {
+          records.forEach((record) => {
+            specimens.push(record.fields);
+          });
+          fetchNextPage();
+        },
+        (err) => {
+          if (err) {
+            reject(err);
+          } else {
+            resolve(specimens);
+          }
+        }
+      );
+  });
+};
 
 //
 // exports.loadAirtable = functions.https.onRequest(async (req, res) => {
@@ -88,9 +102,8 @@ const loadData = () => {
 //   return res.status(200).send(formatted)
 // })
 
-
 exports.loadAirtable = functions.https.onCall(async (dat) => {
-  const fetched = await loadData()
-  const formatted = formatJSON(fetched)
-  return formatted
-})
+  const fetched = await loadData();
+  const formatted = formatJSON(fetched);
+  return formatted;
+});
--- src/data/loadAirtableENV.js
+++ src/data/loadAirtableENV.js
@@ -1,85 +1,99 @@
-import Airtable from 'airtable';
+import Airtable from "airtable";
 
-const airtableKey = process.env.REACT_APP_AIRTABLE_API_KEY
-const baseString = process.env.REACT_APP_AIRTABLE_BASE
+const airtableKey = process.env.REACT_APP_AIRTABLE_PAT;
+const baseString = process.env.REACT_APP_AIRTABLE_BASE;
 
 const formatJSON = (data) => {
   console.log(data);
-  const formatted = []
+  const formatted = [];
   const template = {
-    "uid": "",
-    "url": "",
-    "scientific": "",
-    "common": "",
-    "group": "",
-    "chapter": [],
-    "showstopper": false,
-    "default": false,
-    "week": [],
-    "type": "",
-    "section": "",
-    "sectionIndex": "",
-    "caption": "",
-  }
+    uid: "",
+    url: "",
+    scientific: "",
+    common: "",
+    group: "",
+    chapter: [],
+    showstopper: false,
+    default: false,
+    week: [],
+    type: "",
+    section: "",
+    sectionIndex: "",
+    caption: "",
+  };
   for (let datum of data) {
     const newDatum = {
-      "uid": datum['UID'] ? datum['UID'] : template['UID'],
-      "url": datum['Link'] ? datum['Link'] : template['url'],
-      "scientific": datum['Scientific Name'] ? datum['Scientific Name'] : template['scientific'],
-      "common": datum['Other Name'] ? datum['Other Name'] : template['common'],
-      "group": datum['Group'] ? datum['Group'] : template['group'],
-      "chapter": datum['Chapter'] ? datum['Chapter'] : template['chapter'],
-      "showstopper": datum['Showstopper'] ? datum['Showstopper'] : template['showstopper'],
-      "default": datum['Default'] ? datum['Default'] : template['default'],
-      "week": datum['Week'] ? datum['Week'] : template['week'],
-      "type": datum['Type'] ? datum['Type'] : template['type'],
-      "section": datum['Section'] ? datum['Section'] : template['section'],
-      "sectionIndex": datum['SectionIndex'] ? datum['SectionIndex'] : template['sectionIndex'],
-      "caption": datum['Caption'] ? datum['Caption'] : template['caption'],
-    }
-    formatted.push(newDatum)
+      uid: datum["UID"] ? datum["UID"] : template["UID"],
+      url: datum["Link"] ? datum["Link"] : template["url"],
+      scientific: datum["Scientific Name"]
+        ? datum["Scientific Name"]
+        : template["scientific"],
+      common: datum["Other Name"] ? datum["Other Name"] : template["common"],
+      group: datum["Group"] ? datum["Group"] : template["group"],
+      chapter: datum["Chapter"] ? datum["Chapter"] : template["chapter"],
+      showstopper: datum["Showstopper"]
+        ? datum["Showstopper"]
+        : template["showstopper"],
+      default: datum["Default"] ? datum["Default"] : template["default"],
+      week: datum["Week"] ? datum["Week"] : template["week"],
+      type: datum["Type"] ? datum["Type"] : template["type"],
+      section: datum["Section"] ? datum["Section"] : template["section"],
+      sectionIndex: datum["SectionIndex"]
+        ? datum["SectionIndex"]
+        : template["sectionIndex"],
+      caption: datum["Caption"] ? datum["Caption"] : template["caption"],
+    };
+    formatted.push(newDatum);
   }
-  return formatted
-}
+  return formatted;
+};
 
 const loadData = () => {
-  const base = new Airtable({apiKey: airtableKey}).base(baseString)
+  const base = new Airtable({
+    endpointUrl: "https://api.airtable.com",
+    apiKey: airtableKey,
+  }).base(baseString);
   return new Promise((resolve, reject) => {
-    const specimens = []
-    base('Specimens').select({
+    const specimens = [];
+    base("Specimens")
+      .select({
         fields: [
-          'Scientific Name',
-          'Other Name',
-          'UID',
-          'Link',
-          'Type',
-          'Showstopper',
-          'Default',
-          'Group',
-          'Chapter',
-          'Week',
-          'Section',
-          'SectionIndex',
-          'Caption'
+          "Scientific Name",
+          "Other Name",
+          "UID",
+          "Link",
+          "Type",
+          "Showstopper",
+          "Default",
+          "Group",
+          "Chapter",
+          "Week",
+          "Section",
+          "SectionIndex",
+          "Caption",
         ],
-        view: "Grid view"
-    }).eachPage(function page(records, fetchNextPage) {
-        records.forEach(function(record) {
-            specimens.push(record.fields)
-        });
-        fetchNextPage();
-    }, function done(err) {
-        if (err) {
-              reject(err);
-            } else {
-              resolve(specimens);
-            }
-    })
-  })
-}
+        view: "Grid view",
+      })
+      .eachPage(
+        function page(records, fetchNextPage) {
+          records.forEach(function (record) {
+            specimens.push(record.fields);
+          });
+          fetchNextPage();
+        },
+        function done(err) {
+          if (err) {
+            reject(err);
+          } else {
+            resolve(specimens);
+          }
+        }
+      );
+  });
+};
 
 export const loadAirtable = async () => {
-  const data = await loadData()
-  const formatted = formatJSON(data)
-  return formatted
-}
+  const data = await loadData();
+  const formatted = formatJSON(data);
+  return formatted;
+};
